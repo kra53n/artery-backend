1102c0ee6d37a5891dc1ba2579d436d19d33427b
kra53n <59062496+kra53n@users.noreply.github.com>
2024-04-12 18:32:21 +0300

    Update orders.py

--- artery/api/services/orders.py
+++ artery/api/services/orders.py
@@ -1,4 +1,4 @@
-from ..models import Client, Order, City
+from ..models import Client, Order, Order_Product, City
 
 
 def get_by_client(client_id: int):
@@ -15,13 +15,21 @@ def get_by_client(client_id: int):
     ]
 
     
-def take_order(client_id: int, city_start_id: int):
+def take_order(client_id: int, city_start_id: int, product_id: int):
     client = Client.objects.get(id=client_id)
     city_start = City.objects.get(id=city_start_id)
     city_end = City.objects.get(id=client.city.id)
-    Order(
+    product = Product.objcets.get(id=product_id)
+    order = Order(
         city_start=city_start,
         city_end=city_end,
         status='PROCESS',
         client=client
-   ).save()
+   )
+    order.save()
+    Order_Product(
+        order=order,
+        product=product,
+        # TODO: add amount processing in future
+        amount=0,
+    ).save()
